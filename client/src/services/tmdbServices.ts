7f3dbc050915d47766f7347519e69743a6060500
sid0000007 <143296945+sid0000007@users.noreply.github.com>
2024-08-08 17:11:39 +0530

    Update tmdbServices.ts

--- client/src/services/tmdbServices.ts
+++ client/src/services/tmdbServices.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzRlZjliMmFhOWIxZTI5MWU1YmIyMDI3ODU1NzYyZSIsIm5iZiI6MTcyMjk0MDkyNS4yNzM5MjQsInN1YiI6IjY2OTk2MGYyZWIxYzVkM2E0NmMxOTEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.92mUSbl_eL5Pray0VATJy3BwBhJoHlkLls2pusOnf60";
-
+const TMDB_TOKEN ="please enter your tmdb API token"
 const headers = {
   Authorization: "Bearer " + TMDB_TOKEN,
 };
