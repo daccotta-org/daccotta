a9e02e0512f01b7334d4fd416ef4c55224c315b5
Ashu Kumar <110407868+AshuKr22@users.noreply.github.com>
2024-08-08 19:16:42 +0530

    deleted dist and removed private files

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -1,47 +1,4 @@
-// import React from 'react';
-// import { Navigate, Outlet, useLocation } from 'react-router-dom';
-// import { useAuth } from '../hooks/useAuth';
-// import AuthenticatedLayout from './authenticated-layout';
 
-// const RootLayout: React.FC = () => {
-//   const { user, isOnboarded, isLoaded } = useAuth();
-//   const location = useLocation();
-
-//   if (!isLoaded) {
-//     return <div>Loading...</div>;
-//   }
-
-//   // Paths that don't require authentication
-//   const publicPaths = ['/signin', '/signup'];
-
-//   if (!user) {
-//     // If the user is on a public path, render it
-//     if (publicPaths.includes(location.pathname)) {
-//       return <Outlet />;
-//     }
-//     // Otherwise, redirect to signup
-//     return <Navigate to="/signup" replace />;
-//   }
-
-//   if (!isOnboarded) {
-//     console.log("onboarding ji",isOnboarded);
-    
-//     if (location.pathname === '/onboard') {
-//       return <Outlet />;
-//     }
-//     return <Navigate to="/onboard" replace />;
-//   }
-
-//   // User is authenticated and onboarded
-//   if (publicPaths.includes(location.pathname)) {
-//     // Redirect to home if trying to access signin/signup while authenticated
-//     return <Navigate to="/" replace />;
-//   }
-
-//   return <AuthenticatedLayout />;
-// };
-
-// export default RootLayout;
 import React, { useEffect } from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
