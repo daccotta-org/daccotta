6e1f44f52f83a4fc6f3da586612fd74991f2ba48
Ashu Kumar <110407868+AshuKr22@users.noreply.github.com>
2024-09-21 22:04:06 +0530

    stats take1

---
+++ client/src/lib/firebaseConfigEx.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+}
+//rename this file to firebaseConfig.ts and fill in the values from your firebase project
