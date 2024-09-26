c0e6cc7d7abc1138a675b35267e771e1ba207e90
Ashu Kumar <110407868+AshuKr22@users.noreply.github.com>
2024-09-26 22:49:53 +0530

    updated firebaseConfig

---
+++ client/src/lib/firebaseConfig.ts
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+}
+export const firebaseConfig2 = {
+    apiKey: "AIzaSyDp5LFFF9TU9W1LzB0Cus--lxBawNyBc5Q",
+    authDomain: "mock-daccotta.firebaseapp.com",
+    projectId: "mock-daccotta",
+    storageBucket: "mock-daccotta.appspot.com",
+    messagingSenderId: "586345450139",
+    appId: "1:586345450139:web:84f82ab90882cd0fe4143e",
+}
