3705dea4b2ddcbebe60e027469142654796899af
sid0000007 <mr.siddharth9267963125@gmail.com>
2024-09-27 01:27:04 +0530

    fixed the API route ENV

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-import { firebaseConfig } from "./firebaseConfig"
+import { firebaseConfig2 } from "./firebaseConfig"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
 // Initialize Firebase`
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig2)
 export const auth = getAuth()
 
 export default app
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,22 @@
+import { defineConfig } from 'vite'
 import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+        "@": path.resolve(__dirname, "./src"),
     },
+},
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mock-backend-32tp.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
