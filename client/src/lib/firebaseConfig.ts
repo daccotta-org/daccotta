a39ad9bd621f26d622912cd65ecafef6cf8b9f90
Ashu Kumar <110407868+AshuKr22@users.noreply.github.com>
2024-09-28 14:30:03 +0530

    fix config

--- client/src/lib/firebaseConfig.ts
+++ client/src/lib/firebaseConfig.ts
@@ -6,11 +6,3 @@ export const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_ID,
 }
-export const firebaseConfig2 = {
-    apiKey: "AIzaSyDp5LFFF9TU9W1LzB0Cus--lxBawNyBc5Q",
-    authDomain: "mock-daccotta.firebaseapp.com",
-    projectId: "mock-daccotta",
-    storageBucket: "mock-daccotta.appspot.com",
-    messagingSenderId: "586345450139",
-    appId: "1:586345450139:web:84f82ab90882cd0fe4143e",
-}
