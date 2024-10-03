608e1df1f33a1c1476b4319649cb61f5dc5c574f
DevRish <rishavchatterjee1546@gmail.com>
2024-10-03 13:50:37 +0530

    moved functions in userRoutes to userControllers

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -3,8 +3,13 @@ import User from "../models/User"
 import { verifyToken } from "../middleware/verifyToken"
 import {
     checkEmailExists,
+    checkOnboardedStatus,
     checkUsernameAvailability,
+    completeOnboarding,
+    getOtherUserData,
+    getPersonalUserData,
     searchUsers,
+    updateUserProfile,
 } from "../controllers/userController"
 import ListModel from "../models/List"
 import mongoose from "mongoose"
@@ -16,99 +21,23 @@ const router = Router()
 // Route to get user data
 router.post("/check-email", checkEmailExists)
 // Route to get personal user data
-router.get("/:uid", verifyToken, async (req: Request, res: Response) => {
-    try {
-        const { uid } = req.params
-
-        if (req.user?.uid !== uid) {
-            return res.status(403).json({ error: "Unauthorized" })
-        }
-
-        const user = await User.findById(uid)
-        if (!user) {
-            return res.status(404).json({ error: "User not found" })
-        }
-
-        res.json(user)
-    } catch (error) {
-        console.error("Error fetching user data:", error)
-        res.status(500).json({ error: "Internal server error" })
-    }
-})
+router.get("/:uid", verifyToken, getPersonalUserData)
 
 // Route to get other user data
-router.get("/:uid/other", verifyToken, async (req: Request, res: Response) => {
-    try {
-        const { uid } = req.params
-
-        const user = await User.findById(uid)
-        if (!user) {
-            return res.status(404).json({ error: "User not found" })
-        }
-
-        res.json(user)
-    } catch (error) {
-        console.error("Error fetching user data:", error)
-        res.status(500).json({ error: "Internal server error" })
-    }
-})
+router.get("/:uid/other", verifyToken, getOtherUserData)
 
 // Route to update user profile
 router.put(
     "/:uid/profile",
     verifyToken,
-    async (req: Request, res: Response) => {
-        try {
-            console.log("I am on")
-
-            const { uid } = req.params
-            const updateData = req.body
-
-            if (req.user?.uid !== uid) {
-                return res.status(403).json({ error: "Unauthorized" })
-            }
-
-            const updatedUser = await User.findByIdAndUpdate(uid, updateData, {
-                new: true,
-            })
-            if (!updatedUser) {
-                return res.status(404).json({ error: "User not found" })
-            }
-
-            res.json({
-                message: "Profile updated successfully",
-                user: updatedUser,
-            })
-        } catch (error) {
-            console.error("Error updating profile:", error)
-            res.status(500).json({ error: "Internal server error" })
-        }
-    }
+    updateUserProfile
 )
 
 // Route to check onboarded status
 router.get(
     "/:uid/onboarded",
     verifyToken,
-    async (req: Request, res: Response) => {
-        try {
-            const { uid } = req.params
-
-            if (req.user?.uid !== uid) {
-                return res.status(403).json({ error: "Unauthorized" })
-            }
-
-            const user = await User.findById(uid)
-            if (!user) {
-                return res.status(404).json({ error: "User not found" })
-            }
-
-            res.json({ onboarded: user.onboarded })
-        } catch (error) {
-            console.error("Error checking onboarded status:", error)
-            res.status(500).json({ error: "Internal server error" })
-        }
-    }
+    checkOnboardedStatus
 )
 
 // Route to complete onboarding
@@ -199,66 +128,7 @@ router.get(
 router.post(
     "/:uid/complete-onboarding",
     verifyToken,
-    async (req: Request, res: Response) => {
-        try {
-            const { uid } = req.params
-            const { username, profile_image, topMovies, directors, friends } =
-                req.body
-            if (req.user?.uid !== uid) {
-                return res.status(403).json({ error: "Unauthorized" })
-            }
-
-            const user = await User.findById(uid)
-            if (!user) {
-                return res.status(404).json({ error: "User not found" })
-            }
-
-            // Update the existing top 5 movies list
-            const top5MoviesList = user.lists.find(
-                (list) => list.name === "Top 5 Movies"
-            )
-            if (top5MoviesList) {
-                top5MoviesList.movies = topMovies.map((movie: any) => ({
-                    movie_id: movie.id,
-                    title: movie.title,
-                    poster_path: movie.poster_path,
-                    release_date: movie.release_date,
-                    genre_ids: movie.genre_ids,
-                }))
-            }
-
-            // Replace fake directors list with user's chosen directors
-
-            user.profile_image = profile_image
-
-            user.onboarded = true
-
-            // Add "Onboarded" badge
-            if (!user.badges.includes("Onboarded")) {
-                user.badges.push("Onboarded")
-            }
-
-            await user.save()
-
-            res.json({
-                message: "Onboarding completed successfully",
-                user: user,
-            })
-        } catch (error: unknown) {
-            console.error("Error completing onboarding:", error)
-            if (error instanceof Error) {
-                res.status(500).json({
-                    error: "Internal server error",
-                    details: error.message,
-                })
-            } else {
-                res.status(500).json({
-                    error: "Internal server error",
-                    details: "An unknown error occurred",
-                })
-            }
-        }
-    }
+    completeOnboarding
 )
 
 //Route to check unique username
