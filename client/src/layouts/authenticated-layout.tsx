f5ee50f450d4e485d054643203ed9e3fec84cb2b
sid0000007 <mr.siddharth9267963125@gmail.com>
2025-01-21 00:04:17 +0530

    removed dock and integrated sidebar.

--- client/src/layouts/authenticated-layout.tsx
+++ client/src/layouts/authenticated-layout.tsx
@@ -10,15 +10,15 @@ const AuthenticatedLayout: React.FC = () => {
         <main className="flex h-screen w-full items-center justify-start lg:justify-center bg-black pr-2 mx-0">
             <div className="max-h-screen w-full flex flex-row text-neutral h-full lg:my-4">
                 {/* Sidebar (Navbar, Groups, Bottom) */}
-                <div className=" flex-col h-full hidden lg:flex gap-4">                   
+                <div className=" flex-col h-full  lg:flex gap-4">
                     <Navbar />
                 </div>
                 {/* Main Content */}
                 <div className="w-[90vw] lg:rounded-3xl bg-background flex-1 relative flex justify-center  items-center">
                     <Outlet />
-                    <div className="absolute z-50 block lg:hidden bottom-10">
+                    {/* <div className="absolute z-50 block lg:hidden bottom-10">
                         <DockDemo />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </main>
